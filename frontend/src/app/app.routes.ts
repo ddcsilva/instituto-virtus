ad403c2f0e54830721f6e8692da7c61014479de5
Danilo Silva <danilo.silva@msn.com>
2025-06-05 23:13:25 -0300

    feat: adicionar rotas e componentes para dashboard, alunos, turmas e pagamentos
    - Implementar rotas no arquivo app.routes.ts para redirecionar para o dashboard e carregar componentes para alunos, turmas e pagamentos.
    - Criar componentes para dashboard, alunos, turmas e pagamentos, incluindo arquivos HTML e CSS correspondentes.
    - Garantir que os novos componentes sejam standalone e devidamente configurados com templates e estilos.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+  },
+  {
+    path: 'alunos',
+    loadComponent: () =>
+      import('./features/alunos/alunos/alunos.component').then(
+        (c) => c.AlunosComponent
+      ),
+  },
+  {
+    path: 'turmas',
+    loadComponent: () =>
+      import('./features/turmas/turmas/turmas.component').then(
+        (c) => c.TurmasComponent
+      ),
+  },
+  {
+    path: 'pagamentos',
+    loadComponent: () =>
+      import('./features/pagamentos/pagamentos/pagamentos.component').then(
+        (c) => c.PagamentosComponent
+      ),
+  },
+];
---
+++ frontend/src/app/features/turmas/turmas/turmas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-turmas',
+  standalone: true,
+  imports: [],
+  templateUrl: './turmas.component.html',
+  styleUrl: './turmas.component.css'
+})
+export class TurmasComponent {
+
+}
